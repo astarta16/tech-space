2246dee25c9980093e41639cd7af1fc26a70166b
Nini Gedeshuri <ninigedeshuri@Giorgis-MacBook-Air.local>
2024-08-08 13:42:44 +0400

    add faq section

--- static/js/index.js
+++ static/js/index.js
@@ -22,3 +22,22 @@ function openModal() {
 function closeModal() {
   document.getElementById("loginModal").style.display = "none";
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const faqQuestions = document.querySelectorAll(".faq-question");
+
+  faqQuestions.forEach((question) => {
+    question.addEventListener("click", () => {
+      const answer = question.nextElementSibling;
+      const icon = question.querySelector(".faq-icon");
+
+      if (answer.style.display === "block") {
+        answer.style.display = "none";
+        icon.textContent = "+";
+      } else {
+        answer.style.display = "block";
+        icon.textContent = "-";
+      }
+    });
+  });
+});
