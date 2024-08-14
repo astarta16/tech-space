a3709687c2daf6f091e30c6312b06b5d9b4cd98e
astarta16 <ngedeshuri2001@gmail.com>
2024-08-14 21:57:37 +0400

    add

--- static/js/index.js
+++ static/js/index.js
@@ -40,4 +40,67 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+
+  const form = document.querySelector(".register-form");
+
+  form.addEventListener("submit", function (event) {
+    const errorMessages = form.querySelectorAll(".error-message");
+    errorMessages.forEach((message) => message.remove());
+
+    let isValid = true;
+
+    const name = document.getElementById("name");
+    if (name.value.trim() === "") {
+      showError(name, "Name is required");
+      isValid = false;
+    }
+
+    const surname = document.getElementById("surname");
+    if (surname.value.trim() === "") {
+      showError(surname, "Surname is required");
+      isValid = false;
+    }
+
+    const email = document.getElementById("email");
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!email.value.match(emailPattern)) {
+      showError(email, "Please enter a valid email address");
+      isValid = false;
+    }
+
+    const age = document.getElementById("age");
+    if (age.value <= 0 || age.value === "") {
+      showError(age, "Please enter a valid age");
+      isValid = false;
+    }
+
+    const birthday = document.getElementById("birthday");
+    if (birthday.value === "") {
+      showError(birthday, "Please enter your birthday");
+      isValid = false;
+    }
+
+    const course = document.getElementById("course");
+    if (course.value === "") {
+      showError(course, "Please select a course");
+      isValid = false;
+    }
+
+    const reason = document.getElementById("reason");
+    if (reason.value.trim() === "") {
+      showError(reason, "Please explain your reasons");
+      isValid = false;
+    }
+
+    if (!isValid) {
+      event.preventDefault();
+    }
+  });
+
+  function showError(input, message) {
+    const errorElement = document.createElement("div");
+    errorElement.className = "error-message";
+    errorElement.innerText = message;
+    input.parentNode.appendChild(errorElement);
+  }
 });
